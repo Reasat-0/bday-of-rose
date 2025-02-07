b0b80a6b4d221573ede74fcd53a97869e693f8b9
reasat-0 <reasat.g8@gmail.com>
2025-02-07 23:28:01 +0600

    test

---
+++ custom.js
@@ -0,0 +1,119 @@
+const box_img = document.querySelector(".gift-box-img");
+const box_img_gif = document.querySelector(".gift-box-img-gif");
+const frame_box = document.querySelector(".frame-box");
+const box_wrapper = document.querySelector(".box-wrapper");
+const text_changing_h3 = document.querySelector(".text-changing-h3");
+
+const bubble = () => {
+  let count = 300;
+
+  const container = document.querySelector(".container");
+  const section = document.querySelector(".section");
+  let i = 0;
+
+  while (i < count) {
+    let bubble = document.createElement("i");
+
+    let x = Math.floor(Math.random() * container.clientWidth);
+    let y = Math.floor(Math.random() * container.clientHeight);
+
+    let size = Math.random() * 10;
+
+    bubble.style.left = x + "px";
+    bubble.style.top = y + "px";
+
+    bubble.style.width = 3 + size + "px";
+    bubble.style.height = 3 + size + "px";
+
+    bubble.style.animationDuration = 5 + size + "s";
+    bubble.style.animationDelay = 0 + "s";
+
+    section.appendChild(bubble);
+
+    i++;
+  }
+
+  // setInterval( () => {
+  // 	container.classList.toggle("red-bg");
+  // },5000)
+};
+
+document.addEventListener("load", bubble());
+
+box_img.addEventListener("click", function (e) {
+  e.target.style.cssText =
+    "visibility: hidden;opacity: 0;position: absolute;display: none";
+
+  box_img_gif.style.cssText = "visibility: visible;opacity: 1;position: unset";
+
+  setTimeout(() => {
+    box_img_gif.style = "display:none";
+    frame_box.style = "visibility: visible;opacity: 1;position: unset";
+    frame_box.classList.add("animate", "infinite", frame_box.dataset.ani);
+  }, 6000);
+
+  document.getElementById("player").src = "music/thousand.mp3";
+
+  $(text_changing_h3).fadeOut(2000, () => {
+    text_changing_h3.innerHTML = "Tap on the pages to turn the pages over :)";
+    $(text_changing_h3).fadeIn(2000);
+  });
+  // setInterval(()=>{
+  // 	text_changing_h3.innerHTML = 'Tap on the pages to turn the pages over :)'
+  // },1000)
+});
+
+// Text Animation
+
+let texts_greeting = [
+  "You Know!!!",
+  "It's a special day...",
+  "It's because on this day...",
+  "A Rose was Bloomed",
+  "who (the ROSE) loves to sleep",
+  "more than she cares me...",
+  "But she is so unlucky that",
+  "she is going to get",
+  "A LITTLE SURPRISe..",
+  "from the person whom",
+  "she doesn't even care...",
+  "Just touch The BOX below...",
+];
+
+// const change_texts = (texts,i=0) => {
+// 	if(i < texts.length){
+// 		text_changing_h3.innerHTML = texts[i];
+// 	}
+// }
+
+let j = 0;
+
+document.querySelector(".roses").onclick = function () {
+  $(this).parent().fadeOut();
+  $(this).siblings("span").fadeOut();
+
+  document.getElementById("player").play();
+  setInterval(() => {
+    let new_text = "",
+      i = 0;
+    if (j < texts_greeting.length) {
+      type_the_text(texts_greeting[j], i, new_text);
+      j++;
+    }
+    if (j === texts_greeting.length - 1) {
+      box_wrapper.style.pointerEvents = "all";
+    }
+  }, 6000);
+};
+
+const type_the_text = (text, i, new_text) => {
+  if (i < text.length) {
+    setInterval(() => {
+      new_text += text[i];
+      if (text[i] != undefined) {
+        text_changing_h3.innerHTML = new_text;
+      }
+      i++;
+    }, 200);
+  }
+};
---
+++ page-fold.js
@@ -0,0 +1,92 @@
+var pageLocation = [],
+  lastPage = null;
+($Zz = 0), (gift_box = document.querySelector(".gift-box"));
+
+TweenLite.set(".centerClass", { xPercent: -50, yPercent: -50 });
+TweenLite.set(".pageWrapper", { left: "0", perspective: 1000 });
+TweenLite.set(".page", { transformStyle: "preserve-3d" });
+TweenLite.set(".back", { rotationY: -180 });
+TweenLite.set([".back", ".front"], { backfaceVisibility: "hidden" });
+
+// All pages must have an id assigned to them in the HTML
+$(".page").click(function () {
+  console.log(this.id);
+  if (pageLocation[this.id] === undefined || pageLocation[this.id] == "right") {
+    $Zz = $(".left").length + 1;
+    TweenMax.to($(this), 1, {
+      force3D: true,
+      rotationY: -180,
+      transformOrigin: "-1px top",
+      className: "+=left",
+      z: $Zz,
+      zIndex: $Zz,
+    });
+    TweenLite.set($(this), { className: "-=right" });
+    pageLocation[this.id] = "left";
+  } else {
+    $Zz = $(".right").length + 1;
+    TweenMax.to($(this), 1, {
+      force3D: true,
+      rotationY: 0,
+      transformOrigin: "left top",
+      className: "+=right",
+      z: $Zz,
+      zIndex: $Zz,
+    });
+    TweenLite.set($(this), { className: "-=left" });
+    pageLocation[this.id] = "right";
+  }
+
+  if (this.id === "page24") {
+    document.getElementById("player").src = "music/hbd.mp3";
+  }
+  if (this.id === "page28") {
+    $(gift_box).fadeOut(1000);
+    $(".final_text").fadeIn(2000);
+  }
+  // if(this.id === 'page9'){
+  // 	console.log('---page 9999')
+  // }
+  // if(this.id === 'page10'){
+  // 	console.log('---page 10000')
+  // }
+});
+
+$(".front").hover(
+  function () {
+    TweenLite.to($(this).find(".pageFoldRight"), 0.3, {
+      width: "50px",
+      height: "50px",
+      backgroundImage:
+        "linear-gradient(45deg,  #fefefe 0%,#f2f2f2 49%,#ffffff 50%,#ffffff 100%)",
+    });
+  },
+  function () {
+    TweenLite.to($(this).find(".pageFoldRight"), 0.3, {
+      width: "0px",
+      height: "0px",
+    });
+  }
+);
+
+$(".back").hover(
+  function () {
+    TweenLite.to($(this).find(".pageFoldLeft"), 0.3, {
+      width: "50px",
+      height: "50px",
+      backgroundImage:
+        "linear-gradient(135deg,  #ffffff 0%,#ffffff 50%,#f2f2f2 51%,#fefefe 100%)",
+    });
+  },
+  function () {
+    TweenLite.to($(this).find(".pageFoldLeft"), 0.3, {
+      width: "0px",
+      height: "0px",
+    });
+  }
+);
+
+// setInterval( () => {
+// 	change_texts(texts,i)
+// 	i++;
+// },4000)
